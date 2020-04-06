bb2c234032543d7a767d7f52105fa930534c6ea2
James Holman <james.holman@domain.com.au>
2020-04-06 23:28:18 +1000

    :alembic: scope hoisting

--- dist/src.f69400ca.js
+++ dist/src.f69400ca.js
@@ -3218,7 +3218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35241" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37697" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
